eccf426e05f58863525ebb2cb6f80a988eded6f9
0woy <qkrdbsdk1030@naver.com>
2025-04-03 15:25:26 +0900

    learn: axios & router todo

---
+++ 03_Vue/ch10-axios/router_axios/src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="mt-3">
+    <h3>존재하지 않는 경로</h3>
+    <p>요청 경로: {{ path }}</p>
+  </div>
+</template>
+<script setup>
+import { useRoute } from 'vue-router';
+
+const { path } = useRoute();
+</script>
